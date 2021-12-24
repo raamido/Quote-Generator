0c844d4a440cd3cf6d14da504859524149633680
Rami Ibrahem <ramiabrahem24@gmail.com>
2021-12-25 00:32:52 +0200

    add loading spinner & fix long-text bug

--- app.js
+++ app.js
@@ -3,15 +3,22 @@ const quoteText = document.getElementById("quote__text");
 const quoteTeller = document.getElementById("quote__teller");
 const TwitterShareButton = document.getElementById("t-button");
 const newQuoteButton = document.getElementById("cta-button");
+const spinner = document.getElementById("spinner");
 
 const getQuote = async () => {
+  loading();
   const API_URL = "http://api.quotable.io/random";
   const response = await fetch(API_URL);
   const quote = await response.json();
   try {
+    complete();
     quoteText.textContent = quote.content;
     quoteTeller.textContent = quote.author;
-    quote.length > 90 ? quoteText.classList.toggle("long-text") : null;
+    if (quote.length > 90) {
+      quoteText.classList.add("long-text");
+    } else {
+      quoteText.classList.remove("long-text");
+    }
   } catch (error) {
     console.error(error);
   }
@@ -20,6 +27,16 @@ const getQuote = async () => {
 const tweetQuote = () => {
   const tweetURL = `https://twitter.com/intent/tweet?text="${quoteText.textContent}" - ${quoteTeller.textContent}`;
   window.open(tweetURL, "_blank");
+};
+
+const loading = () => {
+  spinner.hidden = false;
+  card.hidden = true;
+};
+
+const complete = () => {
+  spinner.hidden = true;
+  card.hidden = false;
 };
 
 getQuote();
